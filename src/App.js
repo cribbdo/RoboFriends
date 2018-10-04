c7459b9db3ffca414d547e03768ad513decfb37e
Cribbdo <cribbdo@gmail.com>
2018-10-04 17:30:52 +0200

    Update Prettier

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import { robots } from './robots';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       robots: robots,
       searchfield: ''
-    }
+    };
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
 
   render() {
     const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
     return (
-      <div className='tc'>
+      <div className="tc">
         <h1>Robo Friends Test 2018</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
