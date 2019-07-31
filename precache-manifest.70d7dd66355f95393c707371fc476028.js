d8c3b8c9a4065ccac608db601cba71c2c78a1648
Cribbdo <cribbdo@gmail.com>
2019-07-31 09:22:45 +0200

    Updates

---
+++ precache-manifest.70d7dd66355f95393c707371fc476028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10942355684df5afed61408aae8968fe",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "42cb729dad18f5de87fc",
+    "url": "/robofriends/static/css/main.bc76d6bd.chunk.css"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/robofriends/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "42cb729dad18f5de87fc",
+    "url": "/robofriends/static/js/main.940ebeb4.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
